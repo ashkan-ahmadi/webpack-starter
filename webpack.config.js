da336cb78adda1ed37a15473315013653007b1b1
Ashkan Ahmadi <ashkan.ahmadi88@gmail.com>
2025-05-17 22:03:54 +0200

    Update Webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,20 @@
 const path = require('path')
 
+// https://webpack.js.org/configuration/
 const config = {
-  entry: './src/index.js',
+  // If not set, webpack sets production as the default value for mode.
+  // If you want to change the behavior according to the mode variable inside the webpack.config.js, you have to export a function instead of an object: https://webpack.js.org/configuration/mode/#mode-none
+  mode: 'development',
 
+  // https://webpack.js.org/configuration/devtool/
+  // Recommended choice for development builds with high quality SourceMaps.
+  devtool: 'eval-source-map',
+
+  entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, './dist'),
-    filename: 'script.js',
+    filename: 'bundle.js',
   },
-  mode: 'none',
 }
 
 module.exports = config
